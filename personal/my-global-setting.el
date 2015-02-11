8135db3ab6a59de58a2558fcc9b2690961f8f7f2
Elvin Hou <chhou@broadcom.com>
2015-02-11 15:52:05 +0800

    fix a bug

--- personal/my-global-setting.el
+++ personal/my-global-setting.el
@@ -14,7 +14,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; highlight changes in program mode
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(add-hook 'prog-mode-hook 'highlight-change-mode)
+(add-hook 'prog-mode-hook 'highlight-changes-mode)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; highlight function calls
